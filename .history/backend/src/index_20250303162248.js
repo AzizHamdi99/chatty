831c39c65889aeefc091cd33bf79b75e4499f56a
AzizHamdi99 <ah1848905@gmail.com>
2025-03-03 16:41:38 +0100

    project done

--- .history/backend/src/index_20250303162248.js
+++ .history/backend/src/index_20250303162248.js
@@ -28,14 +28,24 @@ app.use("/api/messages", messageRoutes)
 
 
 const port = process.env.PORT
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+/*if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}*/
+if (process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "..", "frontend", "dist");
+    console.log("Serving frontend from:", frontendPath); // Debugging
+
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
-
 
 
 server.listen(port, () => {
--- .history/backend/src/index_20250303163804.js
+++ .history/backend/src/index_20250303163804.js
@@ -29,11 +29,12 @@ app.use("/api/messages", messageRoutes)
 
 const port = process.env.PORT
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
 }
 
 
